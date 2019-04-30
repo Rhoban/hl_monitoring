7c5a1435abb746122d867f719857891c86a74ee2
medrimonia <medrimonia@gmail.com>
2019-05-01 00:31:59 +0200

    Adding nb_players to TeamManager + adding conveniency getters

--- include/hl_monitoring/team_manager.h
+++ include/hl_monitoring/team_manager.h
@@ -12,14 +12,26 @@ class TeamManager
 public:
   TeamManager();
 
+  int getNbPlayersPerTeam() const;
+
   bool hasTeam(uint32_t team_id) const;
   const TeamConfig& getTeam(uint32_t team_id) const;
+  /**
+   * If team is present in config, retrieve its name, otherwise generate a string 'Team XX'
+   */
+  std::string getTeamName(uint32_t team_id) const;
+  /**
+   * If given player can be found, returns 'X - name', otherwise returns 'Player X'
+   */
+  std::string getPlayerName(uint32_t team_id, uint32_t player_id) const;
 
   void fromJson(const Json::Value& v);
   Json::Value toJson() const;
 
 private:
   std::map<uint32_t, TeamConfig> teams;
+
+  int nb_players_per_team;
 };
 
 }  // namespace hl_monitoring
--- src/hl_monitoring/team_manager.cpp
+++ src/hl_monitoring/team_manager.cpp
@@ -2,12 +2,15 @@
 
 #include <hl_monitoring/utils.h>
 
-#include <iostream>
-
 namespace hl_monitoring
 {
-TeamManager::TeamManager()
+TeamManager::TeamManager() : nb_players_per_team(0)
 {
+}
+
+int TeamManager::getNbPlayersPerTeam() const
+{
+  return nb_players_per_team;
 }
 
 bool TeamManager::hasTeam(uint32_t team_id) const
@@ -20,14 +23,42 @@ const TeamConfig& TeamManager::getTeam(uint32_t team_id) const
   return teams.at(team_id);
 }
 
+std::string TeamManager::getTeamName(uint32_t team_id) const
+{
+  if (hasTeam(team_id))
+  {
+    return getTeam(team_id).getName();
+  }
+  else
+  {
+    return "Team " + std::to_string(team_id);
+  }
+}
+
+std::string TeamManager::getPlayerName(uint32_t team_id, uint32_t player_id) const
+{
+  std::string result;
+  if (hasTeam(team_id) && getTeam(team_id).hasPlayer(player_id))
+  {
+    result = std::to_string(player_id) + " - " + getTeam(team_id).getName(player_id);
+  }
+  else
+  {
+    result = "Player " + std::to_string(player_id);
+  }
+  return result;
+}
+
 void TeamManager::fromJson(const Json::Value& v)
 {
+  readVal(v, "players", &nb_players_per_team);
   teams = readMap<TeamConfig>(v, "teams");
 }
 
 Json::Value TeamManager::toJson() const
 {
   Json::Value v;
+  v["players"] = nb_players_per_team;
   v["teams"] = map2Json(teams);
   return v;
 }

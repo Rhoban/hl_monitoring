2666c5f35727529abb3aa2c390f478eeba2abed4
medrimonia <medrimonia@gmail.com>
2019-05-04 08:20:54 +0200

    Hard-coded change of kick

--- src/hl_monitoring/drawers/arrow_drawer.cpp
+++ src/hl_monitoring/drawers/arrow_drawer.cpp
@@ -29,7 +29,10 @@ void ArrowDrawer::draw(FieldToImgConverter converter, const std::pair<cv::Point3
   {
     double arrow_length = cv::norm(cv::Mat(img_end - img_src));
     double arrow_tip_ratio = arrow_tip_length / arrow_length;
-    cv::arrowedLine(*out, img_src, img_end, color, arrow_thickness, 0, 0, arrow_tip_ratio);
+    // TODO: tmp test
+    //cv::arrowedLine(*out, img_src, img_end, color, arrow_thickness, 0, 0, arrow_tip_ratio);
+    cv::line(*out, img_src, img_end, color, arrow_thickness);
+    cv::drawMarker(*out, img_end, color, cv::MARKER_TILTED_CROSS, 15, 5);
   }
 }
 
